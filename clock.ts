69db14e2fd7c0752340049a1c9b33ebf90f6cf66
lisamc <lisamccormack85@gmail.com>
2019-12-02 09:58:47 +0000

    numbers ok just wrong length'

--- clock.ts
+++ clock.ts
@@ -9,23 +9,22 @@ class Clock {
 
   public toString() {
     // if (this.hours > 24 && this.minutes === 0) { return `0${this.hours % 24}:00` }
-    if(this.hours >= 24) {
       const hrs = this.hours % 24 + Math.floor(this.minutes / 60)
-      if (hrs > 24) {
-        if (hrs % 24 < 10) {
-          return `0${hrs % 24}:0${this.minutes % 24}`
-        } if (hrs % 24 > 10) {
-          return `${hrs % 24}:0${this.minutes % 24}`}
-      } else {
-        return `0${hrs % 24}:${this.minutes % 60}`
-      }
-    }
+      // if (hrs > 24) {
+      //   if (hrs % 24 < 10) {
+          return `${hrs % 24}:${this.minutes % 60}`
+      //   } if (hrs % 24 > 10) {
+      //     return `${hrs % 24}:${this.minutes % 60}`}
+      // } else {
+      //   return `${hrs % 24}:${this.minutes % 60}`
+      // }
+
     // if (this.hours === 24) {return `00:0${this.minutes}`}
-    if (this.minutes === 60) { return `0${this.hours + 1}:00`}
-    const minsToHours = this.minutes / 60
-    if (minsToHours > 24 ) { return `0${Math.floor(minsToHours % 24)}:${this.minutes % 60}`}
-    if (this.minutes > 60) { return `0${Math.floor(this.minutes/60)}:${this.minutes % 60}`}
-    return this.hours > 9 ? `${this.hours}:0${this.minutes}` : `0${this.hours}:0${this.minutes}`
+    // if (this.minutes === 60) { return `0${this.hours + 1}:00`}
+    // const minsToHours = this.minutes / 60
+    // if (minsToHours > 24 ) { return `0${Math.floor(minsToHours % 24)}:${this.minutes % 60}`}
+    // if (this.minutes > 60) { return `0${Math.floor(this.minutes/60)}:${this.minutes % 60}`}
+    // return this.hours > 9 ? `${this.hours}:0${this.minutes}` : `0${this.hours}:0${this.minutes}`
   }
 
 
