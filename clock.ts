99d817ca7122e5b3d4ddff61231be60cb920840a
lisamc <lisamccormack85@gmail.com>
2019-12-04 10:23:20 +0000

    gnarly negative time code

--- clock.ts
+++ clock.ts
@@ -10,17 +10,33 @@ class Clock {
   public toString() {
     const result = [':']
     if (this.hours < 0 ) {
-      result.unshift(`${24 + this.hours}`)
-      result.push(`${this.minutes}`)
+      const negHours = this.hours % 24
+      result.unshift(`${24 + negHours}`.length === 1 ? `0${24 + negHours}` : `${24 + negHours}`)
+      result.push(`${this.minutes}`.length === 1 ? `0${this.minutes}` : `${this.minutes}` )
         return result.join('')
     }
+
     const hrs = this.hours % 24 + Math.floor(this.minutes / 60)
-    const timeHours = `${hrs % 24}`
-    timeHours.length === 1 ? result.unshift(`0${hrs % 24}`) : result.unshift(`${hrs % 24}`)
+    if (this.minutes < -60) { const negMinHours = 24 + Math.floor(this.minutes / 60) + this.hours
+      if (negMinHours > -24) {result.unshift(`${negMinHours}`.length === 1 ? `0${negMinHours}` : `${negMinHours}`)}
+      else {const rollOver = 24 + (negMinHours % 24)
+        result.unshift(`${rollOver}`.length === 1 ? `0${rollOver}` : `${rollOver}`)
+      }
+    }
     const timeMinutes = `${this.minutes % 60}`
-    timeMinutes.length === 1 ? result.push(`0${this.minutes % 60}`) : result.push(`${this.minutes % 60}`)
+    if (parseFloat(timeMinutes) < 0) { const negMins = 60 + parseFloat(timeMinutes)
+      negMins.toString().length === 1 ? result.push(`0${negMins}`) : result.push(`${negMins}`)
+    }
+
+    const timeHours = `${hrs % 24}`
+    if (timeHours.length === 1 && result[0] === ':') {result.unshift(`0${hrs % 24}`) }
+    if (timeHours.length >1 && result[0] === ':') {result.unshift(`${hrs % 24}`)}
+    if (timeMinutes.length === 1 && result[result.length - 1 ] === ':') { result.push(`0${this.minutes % 60}`)}
+    if (timeMinutes.length > 1 && result[result.length - 1 ] === ':') {result.push(`${this.minutes % 60}`)}
     return result.join('')
+
   }
+
 }
 
 
